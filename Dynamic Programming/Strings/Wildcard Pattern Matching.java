b444fad2e7512b1c585d544e4dcda0863c135e34
Avinash Reddy Kovvuri <84344405+Avinash-Reddy-Kovvuri@users.noreply.github.com>
2023-09-23 12:59:42 +0530

    Update Wildcard Pattern Matching.java

--- Dynamic Programming/Strings/Wildcard Pattern Matching.java
+++ Dynamic Programming/Strings/Wildcard Pattern Matching.java
@@ -53,3 +53,62 @@ class Solution {
         }
     }
 }
+
+
+
+(OR) 
+
+    #Tabulation
+
+
+ class Solution {
+    public boolean isMatch(String s, String p) {
+        
+
+        return wildcardMatching(s,p);
+        
+    }
+
+    public boolean isAllStars(String S1, int i) {
+
+    
+    for (int j = 1; j <= i; j++) {
+      if (S1.charAt(j - 1) != '*')
+        return false;
+    }
+    return true;
+  }
+
+  public boolean wildcardMatching(String S1, String S2) {
+
+    int n = S1.length();
+    int m = S2.length();
+
+    boolean dp[][] = new boolean[n + 1][m + 1];
+    dp[0][0] = true;
+
+    for (int j = 1; j <= m; j++) {
+      dp[0][j] = isAllStars(S2, j);
+    }
+    for (int i = 1; i <= n; i++) {
+      dp[i][0] = isAllStars(S1, i);
+    }
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+
+        if (S1.charAt(i - 1) == S2.charAt(j - 1) || S1.charAt(i - 1) == '?' || S2.charAt(j - 1) == '?')
+          dp[i][j] = dp[i - 1][j - 1];
+
+        else {
+          if (S1.charAt(i - 1) == '*' || S2.charAt(j - 1) == '*')
+            dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+
+          else dp[i][j] = false;
+        }
+      }
+    }
+
+    return dp[n][m];
+}
+}
